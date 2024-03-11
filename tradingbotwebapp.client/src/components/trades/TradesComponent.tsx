d9678f8eb9f518a0635eb336f0ce948f755aa7c2
Rubicon-Dev <dev@rubiconsa.com>
2024-03-11 18:34:32 +0200

    added basis for watchlist and trades

--- tradingbotwebapp.client/src/components/trades/TradesComponent.tsx
+++ tradingbotwebapp.client/src/components/trades/TradesComponent.tsx
@@ -1,6 +1,43 @@
+import { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+interface Trades {
+    date: string;
+    message: string;
+    value: number;
+}
+
 function TradesComponent() {
+    const [tradesMessages, setTadesMessages] = useState<Trades[]>();
+
+    useEffect(() => {
+        populateTradesMessagesData();
+    }, []);
+
+    async function populateTradesMessagesData() {
+        //const response = await fetch('weatherforecast');
+        //const data = await response.json();
+        const data: Trades[] = [
+            { date: "2024-03-11", message: "Buy: BTC", value: 22 },
+            { date: "2024-03-10", message: "Buy: ETH", value: 22 },
+            { date: "2024-03-09", message: "Sell: BTC", value: 22 },
+            { date: "2024-03-08", message: "Buy: SOL", value: 22 },
+            { date: "2024-03-07", message: "Sell: SOL", value: 22 }
+        ];
+        setTadesMessages(data);
+    }
+
+
   return (
-    <p>Hello world, trades!</p>
+      <div className="card">
+          <div className="card-body">
+              <h2 className="card-title text-left">Trades</h2>
+              {tradesMessages?.map((log, index) => (
+                  <p key={index}>{log.date}: {log.message}</p>
+              ))}
+          </div>
+      </div>
   );
 }
 
--- tradingbotwebapp.client/src/components/watchlist/WatchListComponent.tsx
+++ tradingbotwebapp.client/src/components/watchlist/WatchListComponent.tsx
@@ -1,7 +1,24 @@
 function WatchListComponent() {
-  return (
-    <p>Hello world, watchlist!</p>
-  );
+    return (
+        <div>
+            <div className="heading-padding">
+                <h2>Watchlist</h2>
+            </div>
+            <div className="row">
+                <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+                    <p className="label">BTC</p>
+                    <p className="label">ETH</p>
+                    {/* Add more labels as needed */}
+                </div>
+                <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+                    <p className="">$72000</p>
+                    <p className="">$2000</p>
+                    {/* Add more labels as needed */}
+                </div>
+            </div>
+            <button type="button" className="btn btn-primary">Add More</button>
+        </div>
+    );
 }
 
 export default WatchListComponent;
